5e97e402d6d2365ba9d2df0c2d230aafde94e2c3
yazin24 <yazinconiendo@gmail.com>
2023-05-10 14:52:38 +0800

    .

--- frontend-display/src/Add.js
+++ frontend-display/src/Add.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Add = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+
+    try{
+      await axios.post('http://localhost:9000/employee/add', {firstName, lastName, age});
+      alert('Employee Has Been Added Successfully!')
+      navigate('/');
+    }catch(err) {
+      console.error(err)
+    }
+  
+  }
+
   return (
-    <div>Add</div>
+    <div className='form'>
+      <form onSubmit={onSubmit}>
+      <label>Fist Name</label>
+      <input type='text'/>
+      <label>Last Name</label>
+      <input type='text'/>
+      <label>Age</label>
+      <input type='number' className='age'/>
+      <button type='submit' className='add'>Add</button>
+      </form>
+    </div>
   )
 }
 
--- frontend-display/src/Home.js
+++ frontend-display/src/Home.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
+
+  // const [firstName, setFirstname] = useState('');
+  // const [lastName, setLastName] = useState('');
+  // const [age, setAge] = useState('');
+  const [data, setData] = useState([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:9000/employee/')
+      setData(response.data);
+    }catch(err) {
+      console.error(err);
+
+    }
+  };
+  fetchData();
+
+},[]);
+
+const navigate = useNavigate();
+
+const addPage = () => {
+  navigate('/add')
+}
+
   return (
     <div>
-      
+      <button className='addButton' onClick={addPage}>Add</button>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key = {item.id}>
+            <td>{item.firstName}</td>
+            <td>{item.lastName}</td>
+            <td>{item.age}</td>
+          </tr>
+          ))}
+          
+        </tbody>
+      </table>
     </div>
   )
 }
