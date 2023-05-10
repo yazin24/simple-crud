b720ff9c6473ef22727e6f8d79edb19564c75533
yazin24 <yazinconiendo@gmail.com>
2023-05-10 12:20:08 +0800

    .

--- frontend-display/src/App.js
+++ frontend-display/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Home.js';
+import Add from './Add.js';
+import Update from './Update.js';
 
 function App() {
   return (
     <div className="App">
         <h2>Simple Crud Operation </h2>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/update' element={<Update/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
